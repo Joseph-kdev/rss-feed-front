ed12995dc811a33da29863a5618e1e96890cfe0d
Joseph-kdev <cjkinuthia5@gmail.com>
2024-05-24 11:13:59 +0300

    added api to url

--- src/services/articles.js
+++ src/services/articles.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "https://blogs-rs-sfeed-back.vercel.app/"
+const baseURL = "https://blogs-rs-sfeed-back.vercel.app/api/blogs"
 
 export const getArticles = async() => {
     const articles = await axios.get(baseURL)
